aaa9b0fcdcfc60cf938dd17f9876a50b9c00ca25
N7 GIL <gilbertoaescobedo@gmail.com>
2022-07-17 15:15:53 -0700

    added readme templates. installed inquirer npm. started working on command line title code

---
+++ Assets/to-do-list.md
@@ -0,0 +1,7 @@
+<!-- 1) Create a `.gitignore` file and include `node_modules/` and `.DS_Store/` so that your `node_modules` directory isn't tracked or uploaded to GitHub. Be sure to create your `.gitignore` file before installing any npm dependencies. -->
+
+<!-- 2) Make sure that your repo includes a `package.json` with the required dependencies. You can create one by running `npm init` when you first set up the project, before installing any dependencies. -->
+
+<!-- 3) Include a video of the typical user flow through your application. This includes views of the prompts and the responses after their selection. -->
+
+4) 
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your project title?", "Who is the target audience for the application?", "What problem does it solve?", 'What did you learn?'];
+
+console.log(questions[0]);
+
+const answerTitle = process.argv.slice(2);
+
+console.log(answerTitle);
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+
+
+    // console.log(writeToFile);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
